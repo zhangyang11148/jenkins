f2b7f50cb67652d52e47bc569038165c985ab501
zhangyang11148 <1554307516@qq.com>
2017-01-20 11:34:45 +0800

    Initial Commit

---
+++ jenkins/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  jenkins
+//
+//  Created by 张阳 on 17/1/20.
+//  Copyright © 2017年 张阳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ jenkins/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  jenkins
+//
+//  Created by 张阳 on 17/1/20.
+//  Copyright © 2017年 张阳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
